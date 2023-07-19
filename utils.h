5d4d9365eb2d49460e31975c95183d4166490a01
Erica Ribeiro <66982080+ericarfs@users.noreply.github.com>
2023-07-18 22:07:25 -0300

    Delete utils.h

--- utils.h
+++
@@ -1,9 +0,0 @@
-#ifndef UTILS_H
-#define UTILS_H
-
-void str_overwrite_stdout(); // Funcao que atualiza a tela
-void str_trim_lf(char* arr, int length); // Funcao que substitui o ultimo caracter de uma string de '\n' por '\0'
-void sigintHandler(int sig_num); // Funcao para pegar o ctrl_c
-void sendCom(char *entrada, char *comando, char *mensagem); // Funcao responsavel por separar o comando inserido e o resto da mensagem
-
-#endif
